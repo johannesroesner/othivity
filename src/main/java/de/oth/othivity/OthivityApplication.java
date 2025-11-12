7559f4f21b1d957e2b1ac014581fb9aa4f52d371
johannesroesner <roesnerj@icloud.com>
2025-11-12 23:49:11 +0100

    feat: project structure init

---
+++ src/main/java/de/oth/othivity/OthivityApplication.java
@@ -0,0 +1,13 @@
+package de.oth.othivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OthivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OthivityApplication.class, args);
+	}
+
+}
