0dea6043aa892c3c6ada23e72b750999b0426782
Sebastian Moritz <sebastian.moritz@mekmedia.com>
2025-11-24 20:55:10 +0100

    added: comments

--- src/main/java/de/oth/othivity/service/impl/ProfileServiceImpl.java
+++ src/main/java/de/oth/othivity/service/impl/ProfileServiceImpl.java
@@ -88,6 +88,7 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Override
     public void deleteProfile(Profile profile) {
+        // TODO SBM delete profile not user - OAuth
         userRepository.delete(profile.getUser());
     }
 
