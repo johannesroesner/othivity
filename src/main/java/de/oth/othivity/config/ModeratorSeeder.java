5da2677e757ee123efa293e1a44c4e953ef930f3
johannesroesner <roesnerj@icloud.com>
2026-01-09 19:21:28 +0100

    fix: added test user for assignment requirements

--- src/main/java/de/oth/othivity/config/ModeratorSeeder.java
+++ src/main/java/de/oth/othivity/config/ModeratorSeeder.java
@@ -25,24 +25,44 @@ public class ModeratorSeeder {
 
         return args -> {
 
+            User moderator = new User();
+            moderator.setEmail("moderator@example.com");
+            moderator.setPassword(passwordEncoder.encode(moderatorPassword));
+            userRepository.save(moderator);
+
+            Profile moderatorProfile = new Profile();
+            moderatorProfile.setFirstName("moderator");
+            moderatorProfile.setLastName("moderator");
+            moderatorProfile.setUsername("moderator");
+            Email moderatorEmail = new Email("moderator@example.com");
+            moderatorEmail.setVerified(true);
+            moderatorProfile.setEmail(moderatorEmail);
+            moderatorProfile.setAboutMe("moderator");
+            moderatorProfile.setRole(Role.MODERATOR);
+            moderatorProfile.setLanguage(Language.ENGLISH);
+            moderatorProfile.setUser(moderator);
+            moderatorProfile.setSetupComplete(true);
+            profileRepository.save(moderatorProfile);
+
+            // added a regular user for assigment requirements
             User user = new User();
-            user.setEmail("moderator@example.com");
-            user.setPassword(passwordEncoder.encode(moderatorPassword));
+            user.setEmail("user@example.com");
+            user.setPassword(passwordEncoder.encode("password"));
             userRepository.save(user);
 
-            Profile profile = new Profile();
-            profile.setFirstName("moderator");
-            profile.setLastName("moderator");
-            profile.setUsername("moderator");
-            Email email = new Email("moderator@example.com");
-            email.setVerified(true);
-            profile.setEmail(email);
-            profile.setAboutMe("moderator");
-            profile.setRole(Role.MODERATOR);
-            profile.setLanguage(Language.ENGLISH);
-            profile.setUser(user);
-            profile.setSetupComplete(true);
-            profileRepository.save(profile);
+            Profile userProfile = new Profile();
+            userProfile.setFirstName("test");
+            userProfile.setLastName("user");
+            userProfile.setUsername("testuser");
+            Email userEmail = new Email("user@example.com");
+            userEmail.setVerified(true);
+            userProfile.setEmail(userEmail);
+            userProfile.setAboutMe("test user");
+            userProfile.setRole(Role.USER);
+            userProfile.setLanguage(Language.ENGLISH);
+            userProfile.setUser(user);
+            userProfile.setSetupComplete(true);
+            profileRepository.save(userProfile);
         };
     }
 }
